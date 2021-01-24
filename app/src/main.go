746ddfe383d63a53afb72145f512bed194c0cf06
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2021-01-24 21:10:25 +0100

    Transformed command line go app into http app

--- app/src/main.go
+++ app/src/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello world")
+}
 
 func main(){
-  fmt.Println("Hello world")
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", homeLink)
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
